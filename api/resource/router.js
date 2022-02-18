550c38073f7e45859d7c26f80d6a368590abe646
Amethyst Willia <zmithic@gmail.com>
2022-02-18 12:49:53 -0600

    Implemented get request to get all resources

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const Resources = require('./model');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    Resources.getAllResources()
+        .then(all => {
+            res.status(200).json(all);
+        })
+        .catch(err => {
+            next(err);
+        })
+});
 
 module.exports = router;
