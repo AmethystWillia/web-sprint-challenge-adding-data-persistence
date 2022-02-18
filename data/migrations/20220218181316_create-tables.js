1fb4b2c3fdc44dae389297791c9b105090359c8f
Amethyst Willia <zmithic@gmail.com>
2022-02-18 12:22:52 -0600

    Set up dropTables

--- data/migrations/20220218181316_create-tables.js
+++ data/migrations/20220218181316_create-tables.js
@@ -25,5 +25,8 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects');
 };
